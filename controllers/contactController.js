995235e7c5c8c6dcd5f8a352a1a3cb3f7cbd2f75
Yomirose <roselinefayomi10@yahoo.comcleargit config --global user.email roselinefayomi10@yahoo.com>
2025-02-28 12:26:20 +0100

    Finished contacts form feacture

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer")
 const { ObjectId } = require("mongodb")
 const sanitizeHtml = require("sanitize-html")
 const petsCollection = require("../db").db().collection("pets")
+const contactsCollection = require("../db").db().collection("contacts")
 
 
 const sanitizeOptions = {
@@ -17,6 +18,18 @@ exports.submitContact = async function(req, res, next) {
         return res.json({message: "Sorry!"})
     }
 
+    if (typeof req.body.name != "string") {
+      req.body.name = ""
+    }
+
+    if (typeof req.body.email != "string") {
+      req.body.email = ""
+    }
+
+    if (typeof req.body.comment != "string") {
+      req.body.comment = ""
+    }
+
     if (!validator.isEmail(req.body.email)) {
         console.log("Invalid email detected")
         return res.json({message: "Sorry!"})
@@ -27,7 +40,8 @@ exports.submitContact = async function(req, res, next) {
         return res.json({message: "Sorry!"})
     }
 
-    const doesPetExist = await petsCollection.findOne({ _id: new ObjectId(req.body.petId)})
+    req.body.petId = new ObjectId(req.body.petId)
+    const doesPetExist = await petsCollection.findOne({ _id: req.body.petId})
 
     if (!doesPetExist) {
         console.log("Pet does not exist!")
@@ -35,6 +49,7 @@ exports.submitContact = async function(req, res, next) {
     }
 
     const ourObject = {
+      petId: req.body.petId,
         name: sanitizeHtml(req.body.name, sanitizeOptions),
         email: sanitizeHtml(req.body.email, sanitizeOptions),
         comment: sanitizeHtml(req.body.comment, sanitizeOptions)
@@ -73,7 +88,9 @@ var transport = nodemailer.createTransport({
         </p>`
       })
 
-      await Promise.all([promise1, promise2])
+      const promise3 = await contactsCollection.insertOne(ourObject)
+
+      await Promise.all([promise1, promise2, promise3])
 
   } catch (err) {
     next(err)
@@ -81,4 +98,21 @@ var transport = nodemailer.createTransport({
 
 
     res.send("Thanks for sending data to us")
+}
+
+exports.viewPetContacts = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    console.log("bad id")
+    return res.redirect("/")
+  }
+
+  const pet = await petsCollection.findOne({_id: new ObjectId(req.params.id)})
+
+  if (!pet) {
+    console.log("Pet does not exist")
+    return res.redirect("/")
+  }
+
+  const contacts = await contactsCollection.find({petId: new ObjectId(req.params.id)}).toArray()
+  res. render("pet-contacts", { contacts, pet })
 }
